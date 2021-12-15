6c37d4d16259094a3156aeb0f79c61dc37098d4b
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2021-12-15 22:50:27 +0800

    update

--- src/features/func-lang2json/FuncLang2json.tsx
+++ src/features/func-lang2json/FuncLang2json.tsx
@@ -1,42 +1,135 @@
-import { Component } from "react";
-import React from "react";
-import { Button } from "@chakra-ui/react";
-
+import { Component, createRef } from "react";
+import {
+    Button,
+    createStandaloneToast,
+    HStack,
+    Table,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr,
+} from "@chakra-ui/react";
+import download from "downloadjs";
 interface State {
-    langFile?: Blob;
-    jsonFile?: Blob;
+    langFile?: File;
+    lang?: string;
+    isLoading?: boolean;
 }
 
+interface jFile {
+    [key: string]: string;
+}
 export default class FuncLang2json extends Component<State> {
+    state: State = { langFile: undefined, lang: undefined, isLoading: false };
 
-    state: State = { langFile: undefined };
+    lang = createRef<HTMLInputElement>();
+    loadBtn = createRef<HTMLButtonElement>();
 
-    lang = React.createRef<HTMLInputElement>();
+    toast = createStandaloneToast();
     setLangFile() {
         if (this.lang === undefined) return;
         if (this.lang.current?.files === null) return;
         this.setState({ ...this.state, langFile: this.lang.current?.files[0] });
     }
 
-    readFile(blob: Blob | undefined) {
-        if (blob === undefined) return;
+    readFile(file: File | undefined) {
+        this.setState({ ...this.state, isLoading: true });
+        if (file === undefined) return;
         const reader = new FileReader();
-        reader.onload = () => { console.log(reader.result); }
-        reader.readAsText(blob, "utf-8");
+        reader.onload = () => {
+            this.setState({
+                ...this.state,
+                lang: reader.result,
+                isLoading: false,
+            });
+        };
+        reader.readAsText(file, "utf-8");
+    }
+
+    generateJson() {
+        if (this.state.lang === undefined) {
+            this.toast({
+                title: "生成文件失败",
+                description: "无法读取文件信息，请检查是否已加载文件！",
+                status: "error",
+                duration: 2000,
+                isClosable: true,
+            });
+            return;
+        }
+
+        const lines = this.state.lang.split("\n");
+        let obj: jFile = {};
+        lines.forEach((line) => {
+            let trimed = line.trim();
+            if (trimed === "") return;
+            if (trimed.startsWith("#")) return;
+            if (trimed.startsWith("//")) return;
+            let arr = line.split("=");
+            obj[arr[0]] = arr[1];
+        });
+
+        let blob = new Blob([JSON.stringify(obj, null, "\t")], {
+            type: "text/json;charset=utf-8",
+        });
+
+        download(blob, "result.json", "text/json;charset=utf-8");
+        this.toast({
+            title: "正在下载",
+            description: "下载已经开始。",
+            status: "success",
+            duration: 1500,
+            isClosable: true,
+        });
     }
 
     render() {
         return (
             <div>
-                <input
-                    type="file"
-                    name="lang"
-                    id="lang"
-                    ref={this.lang}
-                    accept=".lang"
-                    onChange={() => this.setLangFile()}
-                />
-                <Button onClick={() => this.readFile(this.state.langFile)}>test</Button>
+                <HStack spacing={3} justify="center">
+                    <input
+                        type="file"
+                        name="lang"
+                        id="lang"
+                        ref={this.lang}
+                        accept=".lang"
+                        onChange={() => this.setLangFile()}
+                    />
+                    <Button
+                        onClick={() => this.readFile(this.state.langFile)}
+                        colorScheme="blue"
+                        loadingText="读取中..."
+                        ref={this.loadBtn}
+                        isLoading={this.state.isLoading}>
+                        读取文件
+                    </Button>
+                    <Button
+                        onClick={() => this.generateJson()}
+                        colorScheme="green">
+                        下载Json
+                    </Button>
+                </HStack>
+                <Table variant="simple" size="sm">
+                    <Thead>
+                        <Tr>
+                            <Th>Key</Th>
+                            <Th>Value</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {this.state.lang?.split("\n").map((s, index) => {
+                            if (s.trim() === "") return null;
+                            const arr = s.split("=");
+                            return (
+                                <Tr key={index}>
+                                    <Td>{arr[0]}</Td>
+                                    <Td>{arr[1]}</Td>
+                                </Tr>
+                            );
+                        })}
+                    </Tbody>
+                </Table>
             </div>
         );
     }
